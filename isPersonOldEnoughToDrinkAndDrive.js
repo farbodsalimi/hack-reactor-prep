29d25211b7fc5ade0e387be6749f7d1f16597d03
Farbod Salimi <farbod_salimi@yahoo.com>
2017-02-08 11:42:11 -0600

    Module 1: Exercises

--- isPersonOldEnoughToDrinkAndDrive.js
+++ isPersonOldEnoughToDrinkAndDrive.js
@@ -1,9 +1,8 @@
 function isPersonOldEnoughToDrinkAndDrive(person) {
-  if (person) {
-    if((person.age > 20) && (person.age > 16))
-    {
-      return true;
+    if (person) {
+        if ((person.age > 20) && (person.age > 16)) {
+            return true;
+        }
     }
-  }
-  return false;
+    return false;
 }
