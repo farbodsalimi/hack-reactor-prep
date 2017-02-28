bb5cda20642d46d11ffefcaaa0923d8b4e979be8
Farbod Salimi <farbod_salimi@yahoo.com>
2017-02-28 00:29:58 -0600

    Module 1: Exercises

---
+++ filterEvenLengthWords.js
@@ -0,0 +1,7 @@
+function filterEvenLengthWords(words) {
+  var filteredAry = words.filter(function(e) { return e.length % 2 === 0 })
+  return filteredAry;
+}
+
+var output = filterEvenLengthWords(['word', 'words', 'word', 'words']);
+console.log(output); // --> ['word', 'word']
