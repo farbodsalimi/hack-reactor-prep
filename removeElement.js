1eec4444091b091b9b5c97075b586512564cff94
Farbod Salimi <farbod_salimi@yahoo.com>
2017-03-02 17:53:19 -0600

    Module 1: Exercises

---
+++ removeElement.js
@@ -0,0 +1,7 @@
+function removeElement(array, discarder) {
+  var filteredAry = array.filter(function(e) { return e !== discarder })
+  return filteredAry;
+}
+
+var output = removeElement([1, 2, 3, 2, 1], 2);
+console.log(output); // --> [1, 3, 1]
