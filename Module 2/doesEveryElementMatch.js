7810b2c417edf9c5eb1d830c5a36b8a9f7eae3ec
Farbod Salimi <farbod_salimi@yahoo.com>
2017-03-17 03:04:12 -0500

    Module 1: Exercises

---
+++ Module 2/doesEveryElementMatch.js
@@ -0,0 +1,24 @@
+// Note: This is a simple implementation of the standard Array method "every()":
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/every
+function every(array, callbackFunction) {
+  var doesEveryElementMatch = true;
+  array.forEach(function(element) {
+    doesEveryElementMatch = callbackFunction(element);
+  });
+  return doesEveryElementMatch;
+}
+
+function assertEqual(actual, expected, testName) {
+  if (JSON.stringify(actual) === JSON.stringify(expected)) {
+    return 'passed';
+  }else {
+    return 'FAILED [' + testName + '] Expected "' + expected + '", but got "' + actual + '"';
+  }
+}
+
+var arr = [1,2,3];
+arr = every(arr, function(a) {
+  return a<10;
+});
+
+console.log(assertEqual(arr, true, 'not okay'));
