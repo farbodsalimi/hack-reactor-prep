754fd802cbb10bb8720132178c7f3ff1d764ca2a
Farbod Salimi <farbod_salimi@yahoo.com>
2017-03-23 19:02:51 -0500

    Module 1: Exercises

--- transformFirstAndLast.js
+++ transformFirstAndLast.js
@@ -6,7 +6,9 @@ function transformFirstAndLast(array) {
 
 var array = ['Queen', 'Elizabeth', 'Of Hearts', 'Beyonce'];
 var test = transformFirstAndLast(array);
-console.log(test);function transformFirstAndLast(array) {
+console.log(test);
+
+function transformFirstAndLast(array) {
   var tmp = {};
   tmp[array[0]] = array[array.length-1];
   return tmp;
