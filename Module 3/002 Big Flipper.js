5e96ed41ae378c9c990ab7b6e97c490a9a858188
Farbod Salimi <farbod_salimi@yahoo.com>
2017-03-27 21:42:48 -0500

    Module 3: Exercises

--- Module 3/002 Big Flipper.js
+++ Module 3/002 Big Flipper.js
@@ -1,23 +1,26 @@
 function flipEveryNChars(input, n) {
   var result = '';
-  
-  /*for (var i = 0; i < input.length; i+=5) {
+
+  /*
+  brute force algorithm
+  for (var i = 0; i < input.length; i+=5) {
     result+= input[i+4];
     result+= input[i+3];
     result+= input[i+2];
     result+= input[i+1];
     result+= input[i];
-  }*/
-  
+  }
+  */
+
   var re = new RegExp('.{1,' + n + '}', "g");
   var splited_array = input.match(re);
   for (var i = 0; i < splited_array.length; i++) {
     result += splited_array[i].split('').reverse().join('');
   }
-  
+
   return result;
 }
 
-var input = 'a short example'; 
+var input = 'a short example';
 var output = flipEveryNChars(input, 5);
 console.log(output); // --> ohs axe trelpma
