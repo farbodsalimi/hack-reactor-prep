a980d910bf97af38cf8cebb7cd6621f12839def7
Farbod Salimi <farbod_salimi@yahoo.com>
2017-03-27 21:45:05 -0500

    Module 3: Exercises

--- Module 3/004 Transpose.js
+++ Module 3/004 Transpose.js
@@ -1,7 +1,7 @@
 /*
-Ceboyrz qrfpevcgvba (frr vafgehpgvbaf sbe ubj gb qr-boshfpngr).
-
-You will be given an array that contains two strings. Your job is to create a function that will take those two strings and transpose them, so that the strings go from top to bottom instead of left to right.
+You will be given an array that contains two strings. Your job is to create a
+function that will take those two strings and transpose them, so that the
+strings go from top to bottom instead of left to right.
 
 e.g. transposeTwoStrings(['Hello','World']);
 
