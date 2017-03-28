7500c39404d7124ade60ae5204e4beaaef14ddb0
Farbod Salimi <farbod_salimi@yahoo.com>
2017-03-27 21:45:11 -0500

    Module 3: Exercises

--- Module 3/005 Find the pair.js
+++ Module 3/005 Find the pair.js
@@ -1,5 +1,6 @@
 /*
-Given a list of non-negative integers and a target sum, find a pair of numbers that sums to the target sum.
+Given a list of non-negative integers and a target sum, find a pair of numbers
+that sums to the target sum.
 
 Example:
 
